0c963d6eec3ffef4a0717d0f86c3c55d9d02eb64
Jevgeni Holodkov <jevgeni.holodkov@gmail.com>
2011-04-22 21:18:35 +0300

    added clj files + test templates

---
+++ src/fridaysquiz/krypton_factor/core.clj
@@ -0,0 +1,6 @@
+(ns fridaysquiz.krypton_factor.core)
+
+(defn easy?
+	"Detects, if the specified seq contains occurrence of two adjoining identical subsequences"
+	[l])
+
---
+++ test/fridaysquiz/test/krypton_factor/core.clj
@@ -0,0 +1,6 @@
+(ns fridaysquiz.test.krypton_factor.core
+	(:use [fridaysquiz.krypton_factor.core])
+    (:use [clojure.test]))
+
+(deftest replace-me ;; FIXME: write
+  (is false "No tests have been written."))
